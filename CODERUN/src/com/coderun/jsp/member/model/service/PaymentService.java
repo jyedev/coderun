05319758d911ed05168bca9fd6f29e118ab5c683
parkseoyoung <parkseoyoung@localhost>
2022-03-29 15:47:15 +0900

    결제하면 프리패스 활성 완

--- CODERUN/src/com/coderun/jsp/member/model/service/PaymentService.java
+++ CODERUN/src/com/coderun/jsp/member/model/service/PaymentService.java
@@ -4,21 +4,40 @@ import static com.coderun.jsp.common.mybatis.Template.getSqlSession;
 
 import org.apache.ibatis.session.SqlSession;
 
+import com.coderun.jsp.member.model.dao.MemberDAO;
 import com.coderun.jsp.member.model.dao.PaymentDAO;
 import com.coderun.jsp.member.model.dto.PaymentDTO;
 
 public class PaymentService {
 	
 	private final PaymentDAO paymentDAO;
+	private final MemberDAO memberDAO;
 	
 	public PaymentService() {
 		paymentDAO = new PaymentDAO();
+		memberDAO = new MemberDAO();
 	}
 
 	public int registPayment(PaymentDTO payment) {
 		SqlSession session = getSqlSession();
 		
 		int result = paymentDAO.insertPayment(session, payment);
+		
+		if(result > 0) {
+			session.commit();
+		} else {
+			session.rollback();
+		}
+		
+		session.close();
+		
+		return result;
+	}
+
+	public int updateFreepass(String memberId) {
+		SqlSession session = getSqlSession();
+		
+		int result = memberDAO.updateFreepass(session, memberId);
 		
 		if(result > 0) {
 			session.commit();
