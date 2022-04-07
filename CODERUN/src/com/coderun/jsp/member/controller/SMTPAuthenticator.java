eaf67f7742cb3571160cf1c849552358d4fd767d
rbsla1112 <96724937+rbsla1112@users.noreply.github.com>
2022-04-07 10:09:01 +0900

    Update SMTPAuthenticator.java

--- CODERUN/src/com/coderun/jsp/member/controller/SMTPAuthenticator.java
+++ CODERUN/src/com/coderun/jsp/member/controller/SMTPAuthenticator.java
@@ -8,8 +8,8 @@ public class SMTPAuthenticator extends Authenticator {
 	}
 	
 	public PasswordAuthentication getPasswordAuthentication() {
-		String username = "rbsla1112@gmail.com";
-		String password = "oixgxmequhtfuovd";
+		String username = "이메일";
+		String password = "비밀번호";
 		return new PasswordAuthentication(username, password);
 	}
 }
